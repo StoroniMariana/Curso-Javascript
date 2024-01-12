29f410405e5c6f750dd2ac5926b6e09da551fce6
StoroniMariana <strovoni779@gmail.com>
2024-01-12 12:57:18 -0300

    variáveis
    o que são variáveis

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//como declarar uma variável - nome de algo//
+//quando for número não precisa aspas(""), quando for textos precisar colocar aspas("")//
+//sempre concluir com ";" ao final de cada declação//
+
+let nomeDaVariavel = 9;
+let fruta = "maça";
+
+//Tipos de dados em variáveis//
