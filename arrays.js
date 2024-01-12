d819bb9d7fed25655db54765036a330cc5073f96
StoroniMariana <strovoni779@gmail.com>
2024-01-12 15:24:08 -0300

    arrays
    explorando arrays

---
+++ arrays.js
@@ -0,0 +1,14 @@
+let cargos = ["Administrador", "Médico", "Advogado", "Analista de Sistema"];
+
+//mudar propriedade sem alterar//
+
+cargos[1] = "Contador";
+
+//adicionar propriedades
+
+cargos.push(Devops);
+
+//retirar propriedades
+cargos.pop();
+console.log(cargos);
+
