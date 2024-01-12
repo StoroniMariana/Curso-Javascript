abd7d301401c792df738192a8da02bae8dd39f6b
StoroniMariana <strovoni779@gmail.com>
2024-01-12 15:45:48 -0300

    operadores aritméticos
    aprendendo sobre operações aritméticas e realizando desafio

---
+++ desafio.js
@@ -0,0 +1,8 @@
+let caixas = 2;
+let pirulitos = 20;
+let resultadoMultiplicado = caixas * pirulitos;
+console.log(resultadoMultiplicado);
+
+let amigos = 5;
+let resultadoDividido = resultadoMultiplicado / amigos;
+console.log(resultadoDividido);
---
+++ operadoresaritmeticos.js
@@ -0,0 +1,22 @@
+//soma
+
+10 + 10
+
+//subtrair
+
+10 - 5
+
+//multiplicar
+
+10 * 5
+
+//dividir
+
+10 / 5
+
+//utilizando dentro de variavéis
+
+let calculo = 10;
+let calculo2 = 5;
+let resultadoMultiplicado = calculo * calculo2;
+console.log(resultadoMultiplicado);
