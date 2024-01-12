fa7065c163b8ff9ec8289d0cf29bb85d8e26066b
StoroniMariana <strovoni779@gmail.com>
2024-01-12 13:52:26 -0300

    variáveis
    aprendendo sobre variáveis

--- javascript.js
+++ javascript.js
@@ -6,3 +6,32 @@ let nomeDaVariavel = 9;
 let fruta = "maça";
 
 //Tipos de dados em variáveis//
+//strings são textos//
+
+let nome = "Mariana";
+
+//integers são números inteiros//
+let idade = 36;
+
+
+//floats são números quebrados//
+let contaBancaria = 765.90;
+
+//variável boleano que vai ser se é verdade ou mentira - verdadeiro ou falso - sim ou não//
+//true
+//false
+let MarianaCabeloLoiro = false;
+
+//Arrays - contém vários valores//
+let frutas = ["Maça", "Banana", "Laranja"];
+
+//Acessar um item expecífico dentro de um array
+ frutas[0];
+
+//objetos
+let xícaras = {cor: "branco", material: "porcelana", peso: 100};
+
+//acessando propriedade de um objeto - só colocar o ponto
+xícaras.peso
+
+
